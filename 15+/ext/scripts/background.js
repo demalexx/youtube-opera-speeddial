27d8b9e728268f7a122e84693785c1e1a63f8a45
Aleksey Demidyuk <demalexx@gmail.com>
2014-10-27 14:29:49 +0800

    - Converted to Opera 15+ extension; - New release 0.6.

---
+++ 15+/ext/scripts/background.js
@@ -0,0 +1,125 @@
+window.addEventListener('load', onLoad, false);
+
+function onLoad() {
+    tryUpdateData();
+}
+
+function switch_mode(mode) {
+    // Reformat content depending on given mode:
+    // d - disabled, e.g. when Youtube is unavailable, or user is not logged-in;
+    // i - items, there are items to show.
+    var center_logo = document.querySelector('#youtube-center-logo');
+    var center_logo_img = center_logo.querySelector('img');
+    var content = document.querySelector('#content');
+
+    if (mode == 'd') {
+        content.style.display = 'none';
+        center_logo.style.display = 'block';
+        center_logo_img.setAttribute('src', '/icons/youtube-logo-bw.png');
+    }
+    else if (mode == 'i') {
+        center_logo.style.display = 'none';
+        content.style.display = 'block';
+    }
+}
+
+function tryUpdateData() {
+    // Try to update data from Youtube and schedule update no matter what
+    // happened (exceptions etc)
+
+    try {
+        updateData();
+    }
+    finally {
+        setTimeout(tryUpdateData, 5 * 60 * 1000);
+    }
+}
+
+function updateData() {
+    // Get data from Youtube and refresh SpeedDial content
+
+    function loadCallback(event) {
+        var response_container = document.createElement('html');
+        response_container.innerHTML = event.target.responseText;
+
+        var items_els = response_container.querySelectorAll('ul li.feed-item-container');
+        var items = [];
+
+        // There is room only for few items
+        for (var i = 0; i < Math.min(items_els.length, 5); i++) {
+            var cur_item = items_els[i];
+
+            var author = cur_item.querySelector('.branded-page-module-title-text').innerText;
+            var author_img_url = cur_item.querySelector('.branded-page-module-title-link img').getAttribute('data-thumb');
+            var title = cur_item.querySelector('h3 a[title]').getAttribute('title');
+            var thumb_url = cur_item.querySelector('a.yt-fluid-thumb-link img').getAttribute('data-thumb');
+            var duration = cur_item.querySelector('.video-time').innerText;
+            var is_watched = Boolean(cur_item.querySelector('.watched-badge'));
+
+            if (author_img_url.indexOf('//') == 0) {
+                author_img_url = 'https:' + author_img_url;
+            }
+
+            if (thumb_url.indexOf('//') == 0) {
+                thumb_url = 'https:' + thumb_url;
+            }
+
+            items.push({'author': author,
+                        'author_img_url': author_img_url,
+                        'title': title,
+                        'thumb_url': thumb_url,
+                        'duration': duration,
+                        'is_watched': is_watched});
+        }
+
+        // Most likely user isn't logged-in
+        if (items.length == 0) {
+            switch_mode('d');
+            return;
+        }
+
+        var out_html = '<table>';
+        for (i = 0; i < items.length; i++) {
+            cur_item = items[i];
+
+            out_html += '<tr>';
+            out_html +=     '<td rowspan="2" class="thumb-cell border-bottom ' + (cur_item['is_watched'] ? 'watched' : '') + '" rowspan="2">';
+            out_html +=         '<div class="video-thumb-container">';
+            out_html +=             '<img class="video-thumb" src="' + cur_item['thumb_url'] + '" />';
+            out_html +=             '<span class="duration">' + cur_item['duration'] + '</span>';
+            out_html +=         '</div>';
+            out_html +=     '</td>';
+            out_html +=     '<td class="author-cell">';
+            out_html +=         '<span class="author">' + htmlEncode(cur_item['author']) + '</span>';
+            out_html +=     '</td>';
+            out_html +=     '<td class="author-icon-cell">';
+            out_html +=         '<img class="author-icon" src="' + cur_item['author_img_url'] + '"/>';
+            out_html +=     '</td>';
+            out_html += '</tr>';
+
+            out_html += '<tr>';
+            out_html +=     '<td class="title-cell border-bottom " colspan="2">';
+            out_html +=         '<span class="title">' + htmlEncode(cur_item['title']) + '</span>';
+            out_html +=     '</td>';
+            out_html += '</tr>';
+        }
+        out_html += '</table>';
+
+        var output = document.querySelector('#content');
+        output.innerHTML = out_html;
+        switch_mode('i');
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.timeout = 15000;
+    xhr.addEventListener('load', loadCallback, false);
+    xhr.open('GET', 'https://www.youtube.com/feed/subscriptions', true);
+    xhr.send();
+}
+
+function htmlEncode(text) {
+    var div = document.createElement('div');
+    var text_node = document.createTextNode(text);
+    div.appendChild(text_node);
+    return div.innerHTML;
+}
